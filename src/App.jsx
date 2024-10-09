e9536454453b6588ee8904bd09286b600144f3e2
ezbhai <varundev@gamil.com>
2024-10-09 12:27:44 +0530

    Update dependencies and audit packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './globals.css'
+import Home from './_root/pages/Home'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <main className='flex h-screen'>
+      <Routes>
+        <Route path='/sign-in' element={<SigninForm />} />
+
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
 
